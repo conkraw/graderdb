59a45cc695657c8fa3d52a06118dcc3f9b4e3f83
conkraw <conkraw@gmail.com>
2025-03-18 21:07:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -70,8 +70,7 @@ def main():
 
     column_value_mapping = {
         "Clinical Assessment Form": {"column": "Evaluation", "value": "*Clinical Assessment of Student"},
-        "Clinical Encounter": {"column": "Checklist", "value": "Pediatrics Case Logs"},
-        "Clinical Encounter": {"column": "Checklist", "value": "LIC - Pediatrics Case Logs"},
+        "Clinical Encounter": {"column": "Checklist", "value": "LIC - Pediatric Case Logs"},
         "Observed Handoff": {"column": "Evaluation", "value": "*PEDS Handoff"},
         "Observed HP": {"column": "Evaluation", "value": "*PEDS History Taking & Physical Exam"},
         "Developmental Assessment": {"column": "SOCIAL/EMOTIONAL MILESTONES (choice=Calms down when spoken to or picked up)", "value": "Unchecked"},
