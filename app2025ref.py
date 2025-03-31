5ac56a44c2da8a5f73b97586fef50aa7ce90d5be
conkraw <conkraw@gmail.com>
2025-03-30 21:40:55 -0400

    Update app2025ref.py

--- app2025ref.py
+++ app2025ref.py
@@ -3281,7 +3281,8 @@ def main():
                 for col in ["reflection", "learning_goals", "strengths_lg", "weaknesses_lg"]:
                     if col not in df.columns:
                         df_processed[col] = None
-                
+
+                st.stop()
                 with st.spinner("Processing the new records..."):
                     for idx, row in df_processed.iterrows():
                         feedback = row["all_feedback"]
